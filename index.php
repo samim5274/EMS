c5ca815976c9cc327864eeb1ba0ece2dd2a60e23
Samim Hossen <85032804+samim5274@users.noreply.github.com>
2023-10-13 18:29:12 +0600

    EMS

---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Welcome to e-COM</title>
+</head>
+<body>
+
+<!-- slid section start -->
+<section>
+    <div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+    </div>
+
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="img/1.1.jpg" alt="Los Angeles" class="d-block" style="width:100%">
+            <div class="carousel-caption">
+                <h3 class="display-4">Los Angeles</h3>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="img/1.3.jpg" alt="Chicago" class="d-block" style="width:100%">
+            <div class="carousel-caption">
+                <h3 class="display-4">Los Angeles</h3>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="img/1.2.jpg" alt="New York" class="d-block" style="width:100%">
+            <div class="carousel-caption">
+                <h3 class="display-4">Los Angeles</h3>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+            </div>
+        </div>
+    </div>
+
+    <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+        <span class="carousel-control-next-icon"></span>
+    </button>
+    </div>
+</section>
+
+<!-- slid section start -->
+
+    
+<!-- menu section start -->
+<section>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark px-4">
+        <a class="navbar-brand" href="#">
+            <img src="img/logo2.png" width="40" height="40" alt="">e-COM
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="collapseNavbar">
+		  <ul class="navbar-nav">
+			<li class="nav-item">
+			  <a class="nav-link" href="#">Home</a>
+			</li>
+            <li class="nav-item dropdown">
+			  <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">Fashion</a>
+            <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Men</a></li>
+                    <li><a class="dropdown-item" href="#">Women</a></li>
+                    <li><a class="dropdown-item" href="#">Boy</a></li>
+                    <li><a class="dropdown-item" href="#">Girl</a></li>
+                    <li><a class="dropdown-item" href="#">Shoes</a></li>
+                    <li><a class="dropdown-item" href="#">Bags</a></li>
+                    <li><a class="dropdown-item" href="#">Watch</a></li>
+                    <li><a class="dropdown-item" href="#">Glass</a></li>
+                </ul>
+			</li>	
+            <li class="nav-item dropdown">
+			  <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">Sport</a>
+                <ul class="dropdown-menu">
+                    <li><a href="#" class="dropdown-item">Football</a></li>
+                    <li><a href="#" class="dropdown-item">Cricket</a></li>
+                    <li><a href="#" class="dropdown-item">Batmintor</a></li>
+                    <li><a href="#" class="dropdown-item">Cycling</a></li>
+                    <li><a href="#" class="dropdown-item">Team Sports</a></li>
+                    <li><a href="#" class="dropdown-item">Exercise</a></li>
+                    <li><a href="#" class="dropdown-item">Hoqi</a></li>
+                    <li><a href="#" class="dropdown-item">Basketball</a></li>
+                </ul>
+			</li>
+            <li class="nav-item dropdown">
+			  <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">Electronic</a>
+              <ul class="dropdown-menu">
+                <li><a href="#" class="dropdown-item">TV</a></li>
+                <li><a href="#" class="dropdown-item">Mobail</a></li>
+                <li><a href="#" class="dropdown-item">Computer</a></li>
+                <li><a href="#" class="dropdown-item">Laptop</a></li>
+                <li><a href="#" class="dropdown-item">Light</a></li>
+                <li><a href="#" class="dropdown-item">Audio</a></li>
+                <li><a href="#" class="dropdown-item">Gaming Consol</a></li>
+                <li><a href="#" class="dropdown-item">Projector</a></li>
+                <li><a href="#" class="dropdown-item">Tablet</a></li>
+              </ul>
+			</li>
+            <li class="nav-item dropdown">
+			  <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">Groceries</a>
+              <ul class="dropdown-menu">
+                <li><a href="#" class="dropdown-item">Breakfast</a></li>
+                <li><a href="#" class="dropdown-item">Fruits</a></li>
+                <li><a href="#" class="dropdown-item">Meat</a></li>
+                <li><a href="#" class="dropdown-item">Dairy & Egg</a></li>
+                <li><a href="#" class="dropdown-item">Herbs</a></li>
+                <li><a href="#" class="dropdown-item">Candy</a></li>
+                <li><a href="#" class="dropdown-item">Laundry</a></li>
+              </ul>
+			</li>	
+            <li class="nav-item dropdown">
+			  <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">Health & Beauty</a>
+              <ul class="dropdown-menu">
+                <li><a href="#" class="dropdown-item">Skil Care</a></li>
+                <li><a href="#" class="dropdown-item">Personal Care</a></li>
+                <li><a href="#" class="dropdown-item">Hair Care</a></li>
+                <li><a href="#" class="dropdown-item">Men's Care</a></li>
+                <li><a href="#" class="dropdown-item">Makeup</a></li>
+                <li><a href="#" class="dropdown-item">Beauty Tools</a></li>
+                <li><a href="#" class="dropdown-item">Bathbody</a></li>
+              </ul>
+			</li>
+            <li class="nav-item">
+			  <a class="nav-link" href="#">Login</a>
+			</li>	            
+		  </ul>	
+		</div>
+    </nav>
+</section>
+<!-- menu section end -->
+
+
+<!-- flash deals section start -->
+
+
+<section id="flashDeal" class="text-center">
+    <div class="container">
+        <div class="row">
+            <h5 class="display-4 text-center py-4 top-title">Hot Deals</h5>
+            <div class="col-md-6 col-lg-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="img/2.5.jpg" class="card-img-top" alt="wait for image...">
+                    <div class="card-body">
+                        <h5 class="card-title">Men's Fashion<span class="badge bg-danger">New</span></h5>
+                        <h3 class="card-title">Price:- <del>$22/-</del>$15/-</h3> 
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-warning">Buy Now</a>
+                        <a href="#" class="btn btn-success">Add Card</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="img/2.4.jpg" class="card-img-top" alt="wait for image...">
+                    <div class="card-body">
+                        <h5 class="card-title">Men's Fashion<span class="badge bg-danger">New</span></h5>
+                        <h3 class="card-title">Price:- <del>$22/-</del>$15/-</h3>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-warning">Buy Now</a>
+                        <a href="#" class="btn btn-success">Add Card</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="img/2.3.jpg" class="card-img-top" alt="wait for image...">
+                    <div class="card-body">
+                        <h5 class="card-title">Men's Fashion<span class="badge bg-danger">New</span></h5>
+                        <h3 class="card-title">Price:-<del>$22/-</del> $15/-</h3>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-warning">Buy Now</a>
+                        <a href="#" class="btn btn-success">Add Card</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="img/2.2.jpg" class="card-img-top" alt="wait for image...">
+                    <div class="card-body">
+                        <h5 class="card-title">Men's Fashion<span class="badge bg-danger">New</span></h5>
+                        <h3 class="card-title">Price:- <del>$22/-</del> $15/-</h3>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-warning">Buy Now</a>
+                        <a href="#" class="btn btn-success">Add Card</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- flash deals section end -->
+
+
+</body>
+</html>
